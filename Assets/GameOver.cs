7bf87122d314fac62bf03a524fc986a328f243de
Aree Vanier <gregory@ajayinkingston.com>
2018-01-24 13:21:10 -0500

    Title and text come from playerprefs

---
+++ Assets/GameOver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour {
+
+	public Text title;
+	public Text body;
+
+	void Start(){
+		title.text = PlayerPrefs.GetString ("GOTitle");
+		body.text = PlayerPrefs.GetString ("GOText");
+		if (title.text == "")
+			title.text = "Title";
+		if (body.text == "")
+			body.text = "body";
+	}
+
+	public void Menu(){
+	}
+}
