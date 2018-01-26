8464f1bf02a2de39c5eae6aca432a535db461af5
Aree-Vanier <gregory@ajayinkingston.com>
2018-01-25 23:13:43 -0500

    Added menu with level start buttons and instructions

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
+///Controls Game Over screen
 public class GameOver : MonoBehaviour {
 
+    ///Screen title
 	public Text title;
+    ///Screen content
 	public Text body;
 
 	void Start(){
+        //Get the text from the player prefs
 		title.text = PlayerPrefs.GetString ("GOTitle");
 		body.text = PlayerPrefs.GetString ("GOText");
 		if (title.text == "")
@@ -17,6 +22,8 @@ public class GameOver : MonoBehaviour {
 			body.text = "body";
 	}
 
+    ///Changes to the menu scene
 	public void Menu(){
+        SceneManager.LoadScene(0);
 	}
 }
---
+++ Assets/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+///Controls the menu
+public class Menu : MonoBehaviour {
+    
+    ///Instructions text
+    public GameObject instructions;
+    ///Level select buttons
+    public GameObject startButtons;
+
+    ///Shows instructions, hides start buttons
+    public void ShowInstructions(){
+        instructions.SetActive(true);
+        startButtons.SetActive(false);
+    }
+
+    ///Shows start buttons, hides instructions
+    public void ShowLevels() {
+        startButtons.SetActive(true);
+        instructions.SetActive(false);
+    }
+
+    ///Starts selected level
+    ///buildIndex: The build index of the level to start
+    public void StartLevel(int buildIndex) {
+        SceneManager.LoadScene(buildIndex);
+    }
+}
