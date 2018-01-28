78ffdb77f0e9fc5d27d6797d911931018fe043f9
Aree-Vanier <gregory@ajayinkingston.com>
2018-01-28 12:51:07 -0500

    Added plane control to gameControl, added canopy keybind

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -11,8 +11,10 @@ public class GameControl : MonoBehaviour {
     public Building[] objectives;
     int startObjectiveAmount;
     public PlaneWrapper plane;
+    public float completion;
 
     void Start() {
+        //Singleton Pattern
         if(instance == null) {
             instance = this;
         }
@@ -23,6 +25,7 @@ public class GameControl : MonoBehaviour {
     }
 
     void Update() {
+        ///Temporary list to hold non-destroyed buildings
         List<Building> temp = new List<Building>();
         foreach(Building b in objectives) {
             //If the objective hasn't been destroyed, add it to temp
@@ -32,6 +35,35 @@ public class GameControl : MonoBehaviour {
         }
         //Update objectives
         objectives = temp.ToArray();
+        completion = objectives.Length / startObjectiveAmount;
 
+    }
+
+    ///Calls planeWrapper functions to control the plane
+    void ControlPlane() {
+        //Toggle gear
+        if (InputManager.getButtonUp(InputManager.Button.GEAR)) {
+            plane.ToggleGear();
+        }
+        //Toggle Canopy
+        if (InputManager.getButtonUp(InputManager.Button.CANOPY)) {
+            plane.ToggleCanopy();
+        }
+        //Deprecated because they are useless
+  //      //Toggle Tail Hook
+  //      if (Input.GetKeyUp(KeyCode.H)) {
+  //          plane.ToggleHook();
+  //      }
+  //      //Eject
+		//if (InputManager.getButton(InputManager.Button.EJECT)) {
+		//	plane.Eject();
+		//}
+
+        //Plane physics functions
+        plane.Propel(InputManager.getAxis(InputManager.Axis.THROTTLE));
+		plane.Pitch(InputManager.getAxis(InputManager.Axis.PITCH));
+		plane.Roll(InputManager.getAxis(InputManager.Axis.ROLL));
+		plane.Yaw(InputManager.getAxis(InputManager.Axis.YAW));
+	}
     }
 }
